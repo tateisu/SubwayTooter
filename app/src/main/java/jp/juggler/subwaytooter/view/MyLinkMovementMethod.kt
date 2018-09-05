0fdb544d658e127049154bc661ab40b4ed75f799
tateisu <tateisu@gmail.com>
2018-09-05 23:47:31 +0900

    クラッシュレポート対応

--- app/src/main/java/jp/juggler/subwaytooter/view/MyLinkMovementMethod.kt
+++ app/src/main/java/jp/juggler/subwaytooter/view/MyLinkMovementMethod.kt
@@ -6,8 +6,11 @@ import android.text.method.Touch
 import android.text.style.ClickableSpan
 import android.view.MotionEvent
 import android.widget.TextView
+import jp.juggler.subwaytooter.util.LogCategory
 
 object MyLinkMovementMethod : LinkMovementMethod() {
+	
+	private val log = LogCategory("MyLinkMovementMethod")
 	
 	// 改行より右をタッチしても反応しないようにする
 	override fun onTouchEvent(widget : TextView, buffer : Spannable, event : MotionEvent) : Boolean {
@@ -36,18 +39,26 @@ object MyLinkMovementMethod : LinkMovementMethod() {
 			val line_right = layout.getLineRight(line)
 			if(line_left <= x && x <= line_right) {
 				
-				val offset = layout.getOffsetForHorizontal(line, x.toFloat())
-				
-				val link = buffer.getSpans(offset, offset, ClickableSpan::class.java)
-				if(link != null && link.isNotEmpty()) {
-					
-					if(action == MotionEvent.ACTION_UP) {
-						link[0].onClick(widget)
+				val offset = try{
+					layout.getOffsetForHorizontal(line, x.toFloat())
+				}catch(ex:Throwable){
+					// getOffsetForHorizontal raises error on Xiaomi Mi A1（tissot_sprout）, Android 8.1
+					log.trace(ex,"getOffsetForHorizontal failed.")
+					null
+				}
+
+				if( offset != null){
+					val link = buffer.getSpans(offset, offset, ClickableSpan::class.java)
+					if(link != null && link.isNotEmpty()) {
+						
+						if(action == MotionEvent.ACTION_UP) {
+							link[0].onClick(widget)
+						}
+						if(widget is MyTextView) {
+							widget.linkHit = true
+						}
+						return true
 					}
-					if(widget is MyTextView) {
-						widget.linkHit = true
-					}
-					return true
 				}
 			}
 		}

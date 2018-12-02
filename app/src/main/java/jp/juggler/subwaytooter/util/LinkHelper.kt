1d44aa6612eaf60d730b1e5cfc4a8d0f02459301
tateisu <tateisu@gmail.com>
2018-12-02 17:02:47 +0900

    リファクタ

--- app/src/main/java/jp/juggler/subwaytooter/util/LinkHelper.kt
+++ app/src/main/java/jp/juggler/subwaytooter/util/LinkHelper.kt
@@ -9,9 +9,11 @@ interface LinkHelper {
 	
 	fun findAcctColor(url : String?) : AcctColor? = null
 	
+	// user とか user@host とかを user@host に変換する
+	// nullや空文字列なら ?@? を返す
 	fun getFullAcct(acct : String?) : String {
 		return when {
-			acct == null -> "?@?"
+			acct?.isEmpty() !=false -> "?@?"
 			acct.contains('@') -> acct
 			else -> "$acct@$host"
 		}

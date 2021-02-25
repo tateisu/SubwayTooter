9f000ea2126eddc2cb804c010159f11431924215
tateisu <tateisu@gmail.com>
2021-02-25 10:24:08 +0900

    remove unused module exif. define compile_sdk_version build script variable.

--- exif/src/main/java/it/sephiroth/android/library/exif2/utils/Utils.kt
+++
@@ -1,7 +0,0 @@
-package it.sephiroth.android.library.exif2.utils
-
-internal fun <E> Collection<E>?.notEmpty() : Collection<E>? =
-	if(this?.isNotEmpty() == true) this else null
-
-internal fun <E> List<E>?.notEmpty() : List<E>? =
-	if(this?.isNotEmpty() == true) this else null

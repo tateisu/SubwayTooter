68c3fa8ff8cb9bee6fe1c1a59aa42148ce1d9a55
tateisu <tateisu@gmail.com>
2022-08-11 16:56:21 +0900

    コード整形

--- app/src/main/java/jp/juggler/subwaytooter/ActHighlightWordEdit.kt
+++ app/src/main/java/jp/juggler/subwaytooter/ActHighlightWordEdit.kt
@@ -5,7 +5,6 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.Color
 import android.media.RingtoneManager
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.CompoundButton

4798d22858126d7c552b5b518e395ad2023f7a5c
tateisu <tateisu@gmail.com>
2023-02-10 13:57:07 +0900

    kotlinx-coroutines-play-services をnoFcmから除去

--- app/src/main/java/jp/juggler/subwaytooter/MyAppGlideModule.kt
+++ app/src/main/java/jp/juggler/subwaytooter/MyAppGlideModule.kt
@@ -144,65 +144,6 @@ class MyAppGlideModule : AppGlideModule() {
             .register(APNGDecoder::class.java, APNGDrawable::class.java, ApngTranscoder())
             .register(SVG::class.java, PictureDrawable::class.java, SvgDrawableTranscoder())
             .append(InputStream::class.java, SVG::class.java, SvgDecoder())
-
-        ///////
-        // Animated WebP
-
-//        // We should put our decoder before the build-in decoders,
-//        // because the Downsampler will consume arbitrary data and make the inputstream corrupt
-//        // on some devices
-//        val resources: Resources = context.resources
-//        val bitmapPool: BitmapPool = glide.bitmapPool
-//        val arrayPool: ArrayPool = glide.arrayPool
-//        /* static webp decoders */
-//        val webpDownsampler = WebpDownsampler(
-//            registry.imageHeaderParsers,
-//            resources.getDisplayMetrics(), bitmapPool, arrayPool
-//        )
-//        val bitmapDecoder = AnimatedWebpBitmapDecoder(arrayPool, bitmapPool)
-//        val byteBufferBitmapDecoder = ByteBufferBitmapWebpDecoder(webpDownsampler)
-//        val streamBitmapDecoder = StreamBitmapWebpDecoder(webpDownsampler, arrayPool)
-//        /* animate webp decoders */
-//        val byteBufferWebpDecoder = ByteBufferWebpDecoder(context, arrayPool, bitmapPool)
-//        registry /* Bitmaps for static webp images */
-//            .prepend(
-//                Registry.BUCKET_BITMAP,
-//                ByteBuffer::class.java,
-//                Bitmap::class.java, byteBufferBitmapDecoder
-//            )
-//            .prepend(
-//                Registry.BUCKET_BITMAP,
-//                InputStream::class.java,
-//                Bitmap::class.java, streamBitmapDecoder
-//            ) /* BitmapDrawables for static webp images */
-//            .prepend(
-//                Registry.BUCKET_BITMAP_DRAWABLE,
-//                ByteBuffer::class.java,
-//                BitmapDrawable::class.java,
-//                BitmapDrawableDecoder(resources, byteBufferBitmapDecoder)
-//            )
-//            .prepend(
-//                Registry.BUCKET_BITMAP_DRAWABLE,
-//                InputStream::class.java,
-//                BitmapDrawable::class.java,
-//                BitmapDrawableDecoder(resources, streamBitmapDecoder)
-//            ) /* Bitmaps for animated webp images*/
-//            .prepend(
-//                Registry.BUCKET_BITMAP,
-//                ByteBuffer::class.java,
-//                Bitmap::class.java, ByteBufferAnimatedBitmapDecoder(bitmapDecoder)
-//            )
-//            .prepend(
-//                Registry.BUCKET_BITMAP,
-//                InputStream::class.java,
-//                Bitmap::class.java, StreamAnimatedBitmapDecoder(bitmapDecoder)
-//            ) /* Animated webp images */
-//            .prepend(ByteBuffer::class.java, WebpDrawable::class.java, byteBufferWebpDecoder)
-//            .prepend(
-//                InputStream::class.java,
-//                WebpDrawable::class.java, StreamWebpDecoder(byteBufferWebpDecoder, arrayPool)
-//            )
-//            .prepend(WebpDrawable::class.java, WebpDrawableEncoder())
     }
 
     override fun applyOptions(context: Context, builder: GlideBuilder) {

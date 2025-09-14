3ec34c9f04e6dc0d3a5069879e4dbf7244771e01
tateisu <tateisu@gmail.com>
2025-09-14 13:38:00 +0900

    remove dependenciesInfo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,6 +107,12 @@ android {
         disable += "MissingTranslation"
     }
 
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
 }
 
 dependencies {
